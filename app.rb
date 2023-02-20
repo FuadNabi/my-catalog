47cf90d7f4365220fde280e17a91bc5c05bb4c07
Serg Skol <sps676@gmail.com>
2023-02-20 17:17:35 +0100

    Add main and app classes

---
+++ app.rb
@@ -0,0 +1,63 @@
+class App
+  def initialize
+    @books = []
+  end
+
+  def list_all_books
+    # add code here
+    puts 'List all books'
+  end
+
+  def list_all_music_albums
+    # add code here
+    puts 'List all music albums'
+  end
+
+  def list_all_movies
+    # add code here
+    puts 'List all movies'
+  end
+
+  def list_all_genres
+    # add code here
+    puts 'List all genres'
+  end
+
+  def list_all_labels
+    # add code here
+    puts 'List all labels'
+  end
+
+  def list_all_authors
+    # add code here
+    puts 'List all authors'
+  end
+
+  def list_all_sources
+    # add code here
+    puts 'List all sources'
+  end
+
+  def add_book
+    # add code here
+    puts 'Add book'
+  end
+
+  def add_music_album
+    # add code here
+    puts 'Add music album'
+  end
+
+  def add_movie
+    # add code here
+    puts 'Add movies'
+  end
+
+  def load_data
+    puts 'Load data'
+  end
+
+  def save_data
+    puts 'Save data'
+  end
+end
