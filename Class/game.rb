28e712ebf1a8d4e22ab4c74104e3f716457e2175
Saikat Chakraborty <saikatgpae@gmail.com>
2023-02-21 18:29:24 +0530

    Add test for game class.

--- Class/game.rb
+++ Class/game.rb
@@ -1,22 +1,16 @@
-require './item'
+require_relative './item'
 
 class Game < Item
   attr_reader :multiplayer, :last_played_at, :published_date
 
   def initialize(multiplayer, last_played_at, published_date)
-    super(published_date, false)
+    super(published_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+    @published_date = published_date
   end
 
   def can_be_archived?
     super && @last_played_at > 2
-  end
-
-  def create_json
-    super.merge({
-                  multiplayer: @multiplayer,
-                  last_played_at: @last_played_at
-                })
   end
 end
---
+++ spec/game_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../Class/game'
+
+describe Game do
+  let(:game) { Game.new('yes', '11/22/2022', '01/28/1998') }
+  describe 'New' do
+    it 'should return an instance of Game' do
+      expect(game).to be_an_instance_of Game
+    end
+    it 'should have a multiplayer' do
+      expect(game.multiplayer).to eq('yes')
+    end
+    it 'should have a last_played_at' do
+      expect(game.last_played_at).to eq('11/22/2022')
+    end
+    it 'should have a published_date' do
+      expect(game.published_date).to eq('01/28/1998')
+    end
+    it 'has can_be_archived? method' do
+      expect(game).to respond_to(:can_be_archived?)
+    end
+  end
+end
