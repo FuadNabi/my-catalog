a788f7a5e6ab96f32210dd45b79715f623e1aca4
FuadNabi <fuad.nebiyev95@mail.ru>
2023-02-21 12:08:30 +0400

    create labels and books table

--- Database/schema.sql
+++ Database/schema.sql
@@ -28,5 +28,25 @@ CREATE TABLE "music_albums"(
     "archived" BOOLEAN
 );
 
+CREATE TABLE labels (
+  id serial PRIMARY KEY,
+  title VARCHAR(200) NOT NULL,
+  color VARCHAR(200) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE books (
+  id serial PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  PRIMARY KEY (id)
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (label_id) REFERENCES labels(id),  
+)
+
 ALTER TABLE
     "music_albums" ADD CONSTRAINT "music_albums_genre_foreign" FOREIGN KEY("genre") REFERENCES "genre"("id");
