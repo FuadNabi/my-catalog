c243cc5408c10169c86e305473149309159df7eb
Serg Skol <sps676@gmail.com>
2023-02-21 16:08:51 +0100

    Adjust test

--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -2,8 +2,8 @@ require_relative '../Class/music_album'
 
 describe MusicAlbum do
   context 'Test MusicAlbum Class' do
-    it 'takes one parameters and returns a object' do
-      @music_album = MusicAlbum.new '2002-02-02'
+    it 'takes two parameters and returns a object' do
+      @music_album = MusicAlbum.new '2002-02-02', true
       expect(@music_album).to be_an_instance_of MusicAlbum
     end
   end
